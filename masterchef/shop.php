85b1af016d027f851c17f6d8cae4ebd8ca133a7c
chenfan617 <free830617@gmail.com>
2015-12-21 03:51:00 +0800

    Create shop.php
    未完成shop(圖片位置大小)
    點擊圖片會稍微放大並連結去材料包

---
+++ masterchef/shop.php
@@ -0,0 +1,55 @@
+<?php
+require("config.php");
+?>
+<style type="text/css">
+.game {
+    position: absolute;
+    background-image: url(pics/homefade.png); 
+    background-repeat: no-repeat; 
+    width: 800px;
+    height: 600px;
+    overflow: hidden;
+    padding-left: 30px;
+    padding-top: 30px;
+}
+.chef{
+	width: 150px;
+	height: 100px;
+    position: absolute;
+	left: 420px;
+	top: 110px;
+}
+ul li{
+    list-style-type:none;
+	width:20px;
+}
+</style>
+<head>
+
+<script>
+</script>
+
+</head>
+<html>
+<body>
+<div class="game">
+
+</div>
+<div class="chef">
+<img src="pics\Chef-Clipart.jpg">
+</div>
+
+<div class="bake" >
+
+<table>
+<tr><td><img src ="pics\circles-2.png" style="z-index:1"><img src ="pics\bread_clip_art_13499.jpg" style="z-index:10"></td>
+<td><img src ="pics\circles-2.png" style="z-index:1"><img src ="pics\cake-clipart-4ncEKrATA.png" style="z-index:10"></td></tr>
+<tr><td><img src ="pics\circles-2.png" style="z-index:1"><img src ="pics\Cookie_with_Chocolate_PNG_Clipart_Picture.png" style="z-index:10"></td>
+<td><img src ="pics\circles-2.png" style="z-index:1"><img src ="pics\clipart-pizza-RiAyEoB9T.jpeg" style="z-index:10">
+</tr>
+</table>
+
+</div>
+
+</body>
+</html>
