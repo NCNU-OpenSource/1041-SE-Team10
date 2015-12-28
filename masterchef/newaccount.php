6baf6bf388e932e5c51dd483555f90d9d39b9f37
krismikayliu <krismikayliu@gmail.com>
2015-12-28 10:49:10 +0800

    newaccount modified

--- masterchef/newaccount.php
+++ masterchef/newaccount.php
@@ -27,7 +27,13 @@ if ($uid) {
 	$sql = "insert into user (id, password, nickname, gender, cash) values ('$uid', '$password','$nickname','$gender', '500');";
     $sql2 = "insert into oven (uid, cakeoven, breadoven, pizzaoven, cookieoven) values ('$uid', '0','0','0', '0');";
     $sql3 = "insert into cakeoven (uid, cake1, cake2, cake3, cake4) values ('$uid', '0','0','0', '0');";
+    $sql4 = "insert into breadoven (uid, bread1, bread2, bread3, bread4) values ('$uid', '0','0','0', '0');";
+    $sql5 = "insert into cookieoven (uid, cookie1, cookie2, cookie3, cookie4) values ('$uid', '0','0','0', '0');";
+    $sql6 = "insert into pizzaoven (uid, pizza1, pizza2, pizza3, pizza4) values ('$uid', '0','0','0', '0');";
 	mysqli_query($conn,$sql) or die("MySQL insert message error"); //執行SQL
+    mysqli_query($conn,$sql2) or die("MySQL insert message error"); //執行SQL
+    mysqli_query($conn,$sql3) or die("MySQL insert message error"); //執行SQL
+    mysqli_query($conn,$sql4) or die("MySQL insert message error"); //執行SQL
 	echo "New account added.";
     header("Location:login.php");
 } else {
