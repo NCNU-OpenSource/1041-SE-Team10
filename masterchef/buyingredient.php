035879ee97b035c539891f6964240564f1343fec
krismikayliu <krismikayliu@gmail.com>
2015-12-28 10:33:08 +0800

    buyoven and buyingredient modified

--- masterchef/buyingredient.php
+++ masterchef/buyingredient.php
@@ -8,14 +8,69 @@ $db = 'masterchef';
 $conn = mysqli_connect($host, $user, $pass,$db) or die('Error with MySQL connection'); //跟MyMSQL連線並登入
 mysqli_query($conn,"SET NAMES utf8");
 $uid=$_SESSION['uID'];
-$select="select * from user where `uid`='$uid';";
+?>
+<style type="text/css">
+.game {
+    position: absolute;
+    background-image: url(pics/materialbackground.jpg); 
+    background-repeat: no-repeat; 
+    width: 800px;
+    height: 600px;
+    overflow: hidden;
+    padding-left: 30px;
+    padding-top: 30px;
+	z-index:1;
+	opacity: .5;
+	filter: Alpha(Opacity=50);
+}
+.game h1{
+    padding-left: 30px;
+    padding-top: 30px;
+    color: black;
+}
+
+.Back{
+    width: 150px;
+	height: 100px;
+    position: absolute;
+	left: 750px;
+	top: 600px;
+}
+.bake {
+	position: absolute;
+	left: 80px;
+	top: 90px;
+    z-index: 10;
+    
+    opacity:0.9;
+}
+</style>
+<head>
+</head>
+<html>
+<body>
+<div class="game">
+<?php
+$select="select * from user;";
+$results=mysqli_query($conn,$select);
+$rs2=mysqli_fetch_array($results);
 $select2="select * from cakeoven where `uid`='$uid';";
-if ($ingredient==cake1) {
+if ($ingredient=='cake1') {
+    if ($rs2['cash']>=50){
 	$sql = "update cakeoven set `cake1`=`cake1`+1 where `uid`='$uid';";
     mysqli_query($conn,$sql) or die("MySQL query error"); 
     $sql2 = "update user set `cash`=`cash`-50 where id='$uid';";
 	mysqli_query($conn,$sql2) or die("MySQL query error2"); //執行SQL
-	//header("Location:cakematerial.php");
+    echo "<h1>purchase successful</h1>";
+    }
+    else{
+        echo "<h1>not enough money!</h1>";
+    }
 } else echo "empty message id.";
-
 ?>
+</div>
+<div class="Back" style="z-index:15">
+<a href="cakematerial.php"><img src="pics/unnamed.png" id="back" width="60px" height="auto" ></a>
+</div>
+</body>
+</html>
